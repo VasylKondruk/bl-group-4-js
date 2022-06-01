1f51eb811dbe1c29da46723914bdc98c95c477c2
Vasyl Kondruk <vkondruk16@gmail.com>
2022-06-01 21:57:45 +0300

    homework update

--- js/lesson-1.js
+++ js/lesson-1.js
@@ -9,70 +9,56 @@
 
 
 
-// const countries = [
-//   "Ukraine",
-//   "Poland",
-//   "Croatia",
-//   "Montenegro",
-//   "France",
-//   "USA",
-// ];
+const countries = [
+  "Ukraine",
+  "Poland",
+  "Croatia",
+  "Montenegro",
+  "France",
+  "USA",
+];
 
-// const countriesPrice = [100, 200, 300, 400, 500, 600];
+const countriesPrice = [100, 200, 300, 400, 500, 600];
 
 
-// let userNameRegistration = prompt("Ваше ім'я");
-// let userPassRegistration = prompt('password');
+let userNameRegistration = prompt("Ваше ім'я");
+let userPassRegistration = prompt('password');
 
-// let userName = '';
-// let userPass = '';
+let userName = '';
+let userPass = '';
 
-// do {
-//     userName = prompt("Ваше ім'я при реєстрації");
-//     userPass = prompt('password при реєстрації');
-// } while (userName !== userNameRegistration || userPass !== userPassRegistration);
+do {
+    userName = prompt("Ваше ім'я при реєстрації");
+    userPass = prompt('password при реєстрації');
+} while (userName !== userNameRegistration || userPass !== userPassRegistration);
 
-// console.log('Ваш логін та пароль успішний');
+console.log('Ваш логін та пароль успішний');
 
-// let maxPrice = prompt('Яку суму готові витратити на тур?');
+let maxPrice = prompt('Яку суму готові витратити на тур?');
 
-// if (maxPrice >= 100 && maxPrice < 200) {
-//     alert(`Країна в яку ви можете поїхати ${countries[0]}`)
-// } else if (maxPrice >= 200 && maxPrice < 300) {
-//     alert(`Країни в які ви можете поїхати ${countries.slice(0,2)}`)
-// }else if (maxPrice >= 300 && maxPrice < 400) {
-//     alert(`Країни в які ви можете поїхати ${countries.slice(0,3)}`)
-// } else if (maxPrice >= 400 && maxPrice < 500) {
-//     alert(`Країни в які ви можете поїхати ${countries.slice(0,4)}`)
-// } else if (maxPrice >= 500 && maxPrice < 600) {
-//     alert(`Країни в які ви можете поїхати ${countries.slice(0,5)}`)
-// } else if (maxPrice >= 600) {
-//     alert(`Країни в які ви можете поїхати ${countries}`)
-// } else {
-//     alert('У вас недостатньо коштів на тур')
-// }
+if (maxPrice >= 100 && maxPrice < 200) {
+    alert(`Країна в яку ви можете поїхати ${countries[0]}`)
+} else if (maxPrice >= 200 && maxPrice < 300) {
+    alert(`Країни в які ви можете поїхати ${countries.slice(0,2)}`)
+}else if (maxPrice >= 300 && maxPrice < 400) {
+    alert(`Країни в які ви можете поїхати ${countries.slice(0,3)}`)
+} else if (maxPrice >= 400 && maxPrice < 500) {
+    alert(`Країни в які ви можете поїхати ${countries.slice(0,4)}`)
+} else if (maxPrice >= 500 && maxPrice < 600) {
+    alert(`Країни в які ви можете поїхати ${countries.slice(0,5)}`)
+} else if (maxPrice >= 600) {
+    alert(`Країни в які ви можете поїхати ${countries}`)
+} else {
+    alert('У вас недостатньо коштів на тур')
+}
 
-// let userCountry = prompt('Вкажіть країну в яку хочете поїхати');
-// let userCredits;
+let userCountry = prompt('Вкажіть країну в яку хочете поїхати');
+let userCredits;
 
-// switch (userCountry) {
-//     case 'Ukraine':
-//         userCredits = maxPrice - 100;
-//         break;
-//     case 'Poland':
-//         userCredits = maxPrice - 200;
-//         break;
-//     case 'Croatia':
-//         userCredits = maxPrice - 300;
-//         break;
-//     case 'Montenegro':
-//         userCredits = maxPrice - 400;
-//         break;
-//     case 'France':
-//         userCredits = maxPrice - 500;
-//         break;
-//     case 'USA':
-//         userCredits = maxPrice - 600;
-//         break;
-// }
-// alert(`Тур оплачений, на вашому рахунку залишилось ${userCredits}$`);
+for (let i = 0; i < countries.length; i += 1) {
+    if (userCountry === countries[i]) {
+        userCredits = maxPrice - countriesPrice[i] 
+    }
+}
+
+alert(`Тур оплачений, на вашому рахунку залишилось ${userCredits}$`);
