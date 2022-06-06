e21437fadc9b0a8e3d5b9ca95f07a3f0cf704ebc
Vasyl Kondruk <vkondruk16@gmail.com>
2022-06-06 17:53:55 +0300

    clear vsc

--- js/bl-4.js
+++ js/bl-4.js
@@ -18,47 +18,47 @@ const countries = [
   "USA",
 ];
 
-const countriesPrice = [100, 200, 300, 400, 500, 600];
+// const countriesPrice = [100, 200, 300, 400, 500, 600];
 
 
 // let userNameRegistration = prompt("Ваше ім'я");
 // let userPassRegistration = prompt('password');
 
-let userName = '';
-let userPass = '';
+// let userName = '';
+// let userPass = '';
 
-do {
+// do {
     // userName = prompt("Ваше ім'я при реєстрації");
     // userPass = prompt('password при реєстрації');
-} while (userName !== userNameRegistration || userPass !== userPassRegistration);
+// } while (userName !== userNameRegistration || userPass !== userPassRegistration);
 
-console.log('Ваш логін та пароль успішний');
+// console.log('Ваш логін та пароль успішний');
 
 // let maxPrice = prompt('Яку суму готові витратити на тур?');
 
-if (maxPrice >= 100 && maxPrice < 200) {
-    alert(`Країна в яку ви можете поїхати ${countries[0]}`)
-} else if (maxPrice >= 200 && maxPrice < 300) {
-    alert(`Країни в які ви можете поїхати ${countries.slice(0,2)}`)
-}else if (maxPrice >= 300 && maxPrice < 400) {
-    alert(`Країни в які ви можете поїхати ${countries.slice(0,3)}`)
-} else if (maxPrice >= 400 && maxPrice < 500) {
-    alert(`Країни в які ви можете поїхати ${countries.slice(0,4)}`)
-} else if (maxPrice >= 500 && maxPrice < 600) {
-    alert(`Країни в які ви можете поїхати ${countries.slice(0,5)}`)
-} else if (maxPrice >= 600) {
-    alert(`Країни в які ви можете поїхати ${countries}`)
-} else {
-    alert('У вас недостатньо коштів на тур')
-}
+// if (maxPrice >= 100 && maxPrice < 200) {
+//     alert(`Країна в яку ви можете поїхати ${countries[0]}`)
+// } else if (maxPrice >= 200 && maxPrice < 300) {
+//     alert(`Країни в які ви можете поїхати ${countries.slice(0,2)}`)
+// }else if (maxPrice >= 300 && maxPrice < 400) {
+//     alert(`Країни в які ви можете поїхати ${countries.slice(0,3)}`)
+// } else if (maxPrice >= 400 && maxPrice < 500) {
+//     alert(`Країни в які ви можете поїхати ${countries.slice(0,4)}`)
+// } else if (maxPrice >= 500 && maxPrice < 600) {
+//     alert(`Країни в які ви можете поїхати ${countries.slice(0,5)}`)
+// } else if (maxPrice >= 600) {
+//     alert(`Країни в які ви можете поїхати ${countries}`)
+// } else {
+//     alert('У вас недостатньо коштів на тур')
+// }
 
-let userCountry = prompt('Вкажіть країну в яку хочете поїхати');
-let userCredits;
+// let userCountry = prompt('Вкажіть країну в яку хочете поїхати');
+// let userCredits;
 
-for (let i = 0; i < countries.length; i += 1) {
-    if (userCountry === countries[i]) {
-        userCredits = maxPrice - countriesPrice[i] 
-    }
-}
+// for (let i = 0; i < countries.length; i += 1) {
+//     if (userCountry === countries[i]) {
+//         userCredits = maxPrice - countriesPrice[i] 
+//     }
+// }
 
 // alert(`Тур оплачений, на вашому рахунку залишилось ${userCredits}$`);
