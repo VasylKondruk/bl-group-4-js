29e7b9d9b6a17026f27cc2cd9342c5c2812c4e77
Vasyl Kondruk <vkondruk16@gmail.com>
2022-06-22 18:15:45 +0300

    avtoper-mod-4

---
+++ js/avtoperevirka-4.js
@@ -0,0 +1,29 @@
+function processCall(recipient, onAvailable, onNotAvailable) {
+  // Імітуємо доступність абонента випадковим числом
+  const isRecipientAvailable = Math.random() > 0.5;
+
+  if (!isRecipientAvailable) {
+    onNotAvailable(recipient);
+    return;
+  }
+
+  onAvailable(recipient);
+}
+
+function takeCall(name) {
+  console.log(`З'єднуємо з ${name}, очікуйте...`);
+  // Логіка прийняття дзвінка
+}
+
+function activateAnsweringMachine(name) {
+  console.log(`Абонент ${name} недоступний, залиште повідомлення.`);
+  // Логіка активації автовідповідача
+}
+
+function leaveHoloMessage(name) {
+  console.log(`Абонент ${name} недоступний, записуємо голограму.`);
+  // Логіка запису голограми
+}
+
+processCall('Манго', takeCall, activateAnsweringMachine);
+processCall('Полі', takeCall, leaveHoloMessage);
